d61b4cbf07c1ae7722adab55563b4699d8714547
rubenvillaverde <ruben18villaverde@gmail.com>
2020-01-24 12:39:02 +0100

    Added PostGresSQL enviromental variable and PSYCOPG2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os 
 from db import db #Esto quiere decir del archivo db.py importa todo db.py 
 from flask import Flask
 from flask_restful import Api, reqparse
@@ -13,7 +14,7 @@ from models.store import StoreModel
 
 app=Flask(__name__)
 #La siguiente linea es la que me crea el esquema.
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'ruben'
 api = Api(app)
@@ -37,8 +38,7 @@ api.add_resource(UserRegister, '/register')
 
 
 
-if __name__=='__main__':
-    
+if __name__=='__main__':  
     app.run(port=5000, debug=True)
 
 
