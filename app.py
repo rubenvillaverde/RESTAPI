c75cd1602d42b68dcdb4e82c01bc84b69603d449
rubenvillaverde <ruben18villaverde@gmail.com>
2020-02-14 10:26:43 +0100

    Fixed app name for uwsgi

--- app.py
+++ app.py
@@ -18,14 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite://
 app.config['SQALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'ruben'
 api = Api(app)
-db.init_app(app)
 
-#ponemos un decorador que afecta al method justo de debajo
-#el decorador hace que se ejecute siempre antes que el primer request que se haga a la API, se acual sea
-@app.before_first_request
-def create_tables():
-    db.create_all()
-    #crea todas las tablas, si no existen, que hallamos definidos en los Models mediante db.Model __tablename__ etc...
 
 jwt=JWT(app, authenticate, identify)
 
